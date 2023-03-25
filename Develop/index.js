50f9e2d71e55bd67f7b65da7a5d478a4c83f2ffc
Cole Arrington <119268105+colearrington98@users.noreply.github.com>
2023-03-24 23:02:23 -0500

    Index.js and generate Markdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Which license does your application use?',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+    ]; 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data); 
+    // process.cwd() is the current working directory
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { // inquirer.prompt() returns a promise
+    inquirer.prompt(questions) //
+    .then((inquirerResponses) => { // inquirerResponses is an object
+        console.log("Generating README..."); 
+        writeToFile("README.md", generateMarkdown({...inquirerResponses})); // spread operator
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,83 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+    if (license !== 'None') { 
+        return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`; // if license is not 'None'
+    }
+    return ''; // if license is 'None'
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+    if (license !== 'None') { 
+        return `\n* [License](#license)`; // if license is not 'None'
+    }
+    return ''; // if license is 'None'
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license !== 'None') { 
+        return `## License
+
+This project is licensed under the ${license} license.`; // if license is not 'None'
+    }
+    return ''; // if license is 'None'
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)} 
+
+## Description
+
+${data.description} 
+## Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+${renderLicenseLink(data.license)}
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation} 
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.tests}
+\`\`\`
+
+${renderLicenseSection(data.license)}
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](
 
 `;
 }
